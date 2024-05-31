6a5ed7b8dcc82f095a72ab52aa54ea75de1df5ad
JailtonAraujo <jailtonraneimoney@gmail.com>
2024-05-30 22:55:06 -0300

    hibernate revange working with a instable config

--- src/main/java/com/project/hibernate/Main.java
+++ src/main/java/com/project/hibernate/Main.java
@@ -1,12 +1,17 @@
 package com.project.hibernate;
 
+
+import com.project.hibernate.generated_sources.People;
 import com.project.hibernate.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
+import org.hibernate.tool.hbm2x.Cfg2JavaTool;
+import org.hibernate.tool.hbm2x.POJOExporter;
 
+import java.io.File;
 import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -16,11 +21,13 @@ public class Main {
         try {
             transaction = session.beginTransaction();
 
-            Query query = session.createNativeQuery("select * from people");
+            Query query = session.createQuery("from People", People.class);
             List objs = query.getResultList();
             System.out.println(objs);
         } catch (Exception e) {
             System.err.println(e);
         }
+
+
     }
 }
